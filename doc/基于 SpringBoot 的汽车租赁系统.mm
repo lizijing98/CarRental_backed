2bf470d47cfc9ff11c3ad5f3cbe11018b3bdb6c6
lizijing98 <lizijing98@live.com>
2022-03-11 15:06:20 +0800

    feat: 新增部分文档

---
+++ doc/基于 SpringBoot 的汽车租赁系统.mm
@@ -0,0 +1,129 @@
+
+<map>
+  <node ID="root" TEXT="基于 SpringBoot 的汽车租赁系统">
+    <node TEXT="超级管理员" ID="bGS0W8yYAt" _mubu_text="%3Cspan%3E%E8%B6%85%E7%BA%A7%E7%AE%A1%E7%90%86%E5%91%98%3C/span%3E" STYLE="bubble" POSITION="right">
+      <node TEXT="信息统计" ID="KNZXD93wbW" _mubu_text="%3Cspan%3E%E4%BF%A1%E6%81%AF%E7%BB%9F%E8%AE%A1%3C/span%3E" STYLE="fork"/>
+      <node TEXT="用户信息管理" ID="MfXZ3Fp5oY" _mubu_text="%3Cspan%3E%E7%94%A8%E6%88%B7%E4%BF%A1%E6%81%AF%E7%AE%A1%E7%90%86%3C/span%3E" STYLE="fork">
+        <node TEXT="用户提权" ID="SOfEQQn3BY" _mubu_text="%3Cspan%3E%E7%94%A8%E6%88%B7%E6%8F%90%E6%9D%83%3C/span%3E" STYLE="fork"/>
+        <node TEXT="用户信息修改" ID="DG8a0REZ0E" _mubu_text="%3Cspan%3E%E7%94%A8%E6%88%B7%E4%BF%A1%E6%81%AF%E4%BF%AE%E6%94%B9%3C/span%3E" STYLE="fork"/>
+        <node TEXT="用户信息查看" ID="Z14G9UKsPf" _mubu_text="%3Cspan%3E%E7%94%A8%E6%88%B7%E4%BF%A1%E6%81%AF%E6%9F%A5%E7%9C%8B%3C/span%3E" STYLE="fork"/>
+        <node TEXT="用户封禁" ID="zkGRAM9oNx" _mubu_text="%3Cspan%3E%E7%94%A8%E6%88%B7%E5%B0%81%E7%A6%81%3C/span%3E" STYLE="fork"/>
+      </node>
+      <node TEXT="门店信息管理" ID="SwElJBJuVW" _mubu_text="%3Cspan%3E%E9%97%A8%E5%BA%97%E4%BF%A1%E6%81%AF%E7%AE%A1%E7%90%86%3C/span%3E" STYLE="fork">
+        <node TEXT="门店信息修改" ID="6lGZqYHTG5" _mubu_text="%3Cspan%3E%E9%97%A8%E5%BA%97%E4%BF%A1%E6%81%AF%E4%BF%AE%E6%94%B9%3C/span%3E" STYLE="fork"/>
+        <node TEXT="门店信息查看" ID="S2Q4SEGKZ9" _mubu_text="%3Cspan%3E%E9%97%A8%E5%BA%97%E4%BF%A1%E6%81%AF%E6%9F%A5%E7%9C%8B%3C/span%3E" STYLE="fork"/>
+        <node TEXT="门店禁用" ID="aarYT824az" _mubu_text="%3Cspan%3E%E9%97%A8%E5%BA%97%E7%A6%81%E7%94%A8%3C/span%3E" STYLE="fork"/>
+      </node>
+      <node TEXT="车辆信息管理" ID="3piyXtlrrQ" _mubu_text="%3Cspan%3E%E8%BD%A6%E8%BE%86%E4%BF%A1%E6%81%AF%E7%AE%A1%E7%90%86%3C/span%3E" STYLE="fork">
+        <node TEXT="新增车辆" ID="w4zJrWFkuh" _mubu_text="%3Cspan%3E%E6%96%B0%E5%A2%9E%E8%BD%A6%E8%BE%86%3C/span%3E" STYLE="fork"/>
+        <node TEXT="车辆信息修改" ID="XoSL3H8ArF" _mubu_text="%3Cspan%3E%E8%BD%A6%E8%BE%86%E4%BF%A1%E6%81%AF%E4%BF%AE%E6%94%B9%3C/span%3E" STYLE="fork"/>
+        <node TEXT="车辆信息查看" ID="ETWd7Qpbz8" _mubu_text="%3Cspan%3E%E8%BD%A6%E8%BE%86%E4%BF%A1%E6%81%AF%E6%9F%A5%E7%9C%8B%3C/span%3E" STYLE="fork"/>
+        <node TEXT="车辆禁用" ID="9TvbebM6hC" _mubu_text="%3Cspan%3E%E8%BD%A6%E8%BE%86%E7%A6%81%E7%94%A8%3C/span%3E" STYLE="fork"/>
+      </node>
+      <node TEXT="订单信息管理" ID="r0fiMqauxw" _mubu_text="%3Cspan%3E%E8%AE%A2%E5%8D%95%E4%BF%A1%E6%81%AF%E7%AE%A1%E7%90%86%3C/span%3E" STYLE="fork">
+        <node TEXT="订单查看" ID="BXI0eUXEhG" _mubu_text="%3Cspan%3E%E8%AE%A2%E5%8D%95%E6%9F%A5%E7%9C%8B%3C/span%3E" STYLE="fork"/>
+        <node TEXT="订单统计" ID="ck5lTRu3Sk" _mubu_text="%3Cspan%3E%E8%AE%A2%E5%8D%95%E7%BB%9F%E8%AE%A1%3C/span%3E" STYLE="fork"/>
+        <node TEXT="订单修改" ID="wzpXMFIz9M" _mubu_text="%3Cspan%3E%E8%AE%A2%E5%8D%95%E4%BF%AE%E6%94%B9%3C/span%3E" STYLE="fork"/>
+      </node>
+      <node TEXT="后勤信息管理" ID="5BKTQ7aCa9" _mubu_text="%3Cspan%3E%E5%90%8E%E5%8B%A4%E4%BF%A1%E6%81%AF%E7%AE%A1%E7%90%86%3C/span%3E" STYLE="fork">
+        <node TEXT="维修单查看" ID="3sYLbDcCnv" _mubu_text="%3Cspan%3E%E7%BB%B4%E4%BF%AE%E5%8D%95%E6%9F%A5%E7%9C%8B%3C/span%3E" STYLE="fork"/>
+        <node TEXT="维修单统计" ID="8oTaQYbREn" _mubu_text="%3Cspan%3E%E7%BB%B4%E4%BF%AE%E5%8D%95%E7%BB%9F%E8%AE%A1%3C/span%3E" STYLE="fork"/>
+        <node TEXT="维修单修改" ID="3PQznrCvGU" _mubu_text="%3Cspan%3E%E7%BB%B4%E4%BF%AE%E5%8D%95%E4%BF%AE%E6%94%B9%3C/span%3E" STYLE="fork"/>
+        <node TEXT="事故单查看" ID="79cIpYHBXk" _mubu_text="%3Cspan%3E%E4%BA%8B%E6%95%85%E5%8D%95%E6%9F%A5%E7%9C%8B%3C/span%3E" STYLE="fork"/>
+        <node TEXT="事故单统计" ID="umkk9HB64p" _mubu_text="%3Cspan%3E%E4%BA%8B%E6%95%85%E5%8D%95%E7%BB%9F%E8%AE%A1%3C/span%3E" STYLE="fork"/>
+        <node TEXT="事故单修改" ID="oyaYfRWDBc" _mubu_text="%3Cspan%3E%E4%BA%8B%E6%95%85%E5%8D%95%E4%BF%AE%E6%94%B9%3C/span%3E" STYLE="fork"/>
+      </node>
+      <node TEXT="日志管理" ID="7s1oUoxY8l" _mubu_text="%3Cspan%3E%E6%97%A5%E5%BF%97%E7%AE%A1%E7%90%86%3C/span%3E" STYLE="fork">
+        <node TEXT="日志统计" ID="8Nctoc4EDe" _mubu_text="%3Cspan%3E%E6%97%A5%E5%BF%97%E7%BB%9F%E8%AE%A1%3C/span%3E" STYLE="fork"/>
+        <node TEXT="日志查看" ID="4Rxujs4fEl" _mubu_text="%3Cspan%3E%E6%97%A5%E5%BF%97%E6%9F%A5%E7%9C%8B%3C/span%3E" STYLE="fork"/>
+      </node>
+    </node>
+    <node TEXT="管理员" ID="cgCZnOsnBc" _mubu_text="%3Cspan%3E%E7%AE%A1%E7%90%86%E5%91%98%3C/span%3E" STYLE="bubble" POSITION="right">
+      <node TEXT="用户信息管理" ID="aJwQAquVyn" _mubu_text="%3Cspan%3E%E7%94%A8%E6%88%B7%E4%BF%A1%E6%81%AF%E7%AE%A1%E7%90%86%3C/span%3E" STYLE="fork">
+        <node TEXT="用户信息查看" ID="CBjvmkB7jx" _mubu_text="%3Cspan%3E%E7%94%A8%E6%88%B7%E4%BF%A1%E6%81%AF%E6%9F%A5%E7%9C%8B%3C/span%3E" STYLE="fork"/>
+      </node>
+      <node TEXT="门店信息管理" ID="LV7hcPXHSx" _mubu_text="%3Cspan%3E%E9%97%A8%E5%BA%97%E4%BF%A1%E6%81%AF%E7%AE%A1%E7%90%86%3C/span%3E" STYLE="fork">
+        <node TEXT="门店信息查看" ID="UACNKWzOCR" _mubu_text="%3Cspan%3E%E9%97%A8%E5%BA%97%E4%BF%A1%E6%81%AF%E6%9F%A5%E7%9C%8B%3C/span%3E" STYLE="fork"/>
+        <node TEXT="门店信息修改" ID="oSTqFzE3PM" _mubu_text="%3Cspan%3E%E9%97%A8%E5%BA%97%E4%BF%A1%E6%81%AF%E4%BF%AE%E6%94%B9%3C/span%3E" STYLE="fork"/>
+      </node>
+      <node TEXT="车辆信息管理" ID="lpIoIRXZHO" _mubu_text="%3Cspan%3E%E8%BD%A6%E8%BE%86%E4%BF%A1%E6%81%AF%E7%AE%A1%E7%90%86%3C/span%3E" STYLE="fork">
+        <node TEXT="新增车辆" ID="KnlmHhttPf" _mubu_text="%3Cspan%3E%E6%96%B0%E5%A2%9E%E8%BD%A6%E8%BE%86%3C/span%3E" STYLE="fork"/>
+        <node TEXT="车辆信息修改" ID="MYgxcjwAEz" _mubu_text="%3Cspan%3E%E8%BD%A6%E8%BE%86%E4%BF%A1%E6%81%AF%E4%BF%AE%E6%94%B9%3C/span%3E" STYLE="fork"/>
+      </node>
+      <node TEXT="订单信息管理" ID="Jnh4CG0iFt" _mubu_text="%3Cspan%3E%E8%AE%A2%E5%8D%95%E4%BF%A1%E6%81%AF%E7%AE%A1%E7%90%86%3C/span%3E" STYLE="fork">
+        <node TEXT="订单查看" ID="zeqyOoHLcz" _mubu_text="%3Cspan%3E%E8%AE%A2%E5%8D%95%E6%9F%A5%E7%9C%8B%3C/span%3E" STYLE="fork"/>
+        <node TEXT="订单统计" ID="SkaAQnNUdV" _mubu_text="%3Cspan%3E%E8%AE%A2%E5%8D%95%E7%BB%9F%E8%AE%A1%3C/span%3E" STYLE="fork"/>
+      </node>
+      <node TEXT="后勤信息管理" ID="Q5lZRD66zh" _mubu_text="%3Cspan%3E%E5%90%8E%E5%8B%A4%E4%BF%A1%E6%81%AF%E7%AE%A1%E7%90%86%3C/span%3E" STYLE="fork">
+        <node TEXT="维修单查看" ID="NQFNCQsQOf" _mubu_text="%3Cspan%3E%E7%BB%B4%E4%BF%AE%E5%8D%95%E6%9F%A5%E7%9C%8B%3C/span%3E" STYLE="fork"/>
+        <node TEXT="维修单统计" ID="tE0T0XvG6m" _mubu_text="%3Cspan%3E%E7%BB%B4%E4%BF%AE%E5%8D%95%E7%BB%9F%E8%AE%A1%3C/span%3E" STYLE="fork"/>
+        <node TEXT="提交维修单" ID="NV1ieX6b4f" _mubu_text="%3Cspan%3E%E6%8F%90%E4%BA%A4%E7%BB%B4%E4%BF%AE%E5%8D%95%3C/span%3E" STYLE="fork"/>
+        <node TEXT="事故单查看" ID="ZAfwNISnjX" _mubu_text="%3Cspan%3E%E4%BA%8B%E6%95%85%E5%8D%95%E6%9F%A5%E7%9C%8B%3C/span%3E" STYLE="fork"/>
+        <node TEXT="事故单统计" ID="qtSfo6vOVS" _mubu_text="%3Cspan%3E%E4%BA%8B%E6%95%85%E5%8D%95%E7%BB%9F%E8%AE%A1%3C/span%3E" STYLE="fork"/>
+        <node TEXT="提交事故单" ID="Ak8kJ8A1Im" _mubu_text="%3Cspan%3E%E6%8F%90%E4%BA%A4%E4%BA%8B%E6%95%85%E5%8D%95%3C/span%3E" STYLE="fork"/>
+      </node>
+    </node>
+    <node TEXT="业务员" ID="48bjXM50n7" _mubu_text="%3Cspan%3E%E4%B8%9A%E5%8A%A1%E5%91%98%3C/span%3E" STYLE="bubble" POSITION="right">
+      <node TEXT="用户信息管理" ID="iGYrzbmEzw" _mubu_text="%3Cspan%3E%E7%94%A8%E6%88%B7%E4%BF%A1%E6%81%AF%E7%AE%A1%E7%90%86%3C/span%3E" STYLE="fork">
+        <node TEXT="用户信息查看" ID="1TATkPbm0n" _mubu_text="%3Cspan%3E%E7%94%A8%E6%88%B7%E4%BF%A1%E6%81%AF%E6%9F%A5%E7%9C%8B%3C/span%3E" STYLE="fork"/>
+      </node>
+      <node TEXT="门店信息管理" ID="2Eaftirj9H" _mubu_text="%3Cspan%3E%E9%97%A8%E5%BA%97%E4%BF%A1%E6%81%AF%E7%AE%A1%E7%90%86%3C/span%3E" STYLE="fork">
+        <node TEXT="门店信息查看" ID="4MP9ueY5PI" _mubu_text="%3Cspan%3E%E9%97%A8%E5%BA%97%E4%BF%A1%E6%81%AF%E6%9F%A5%E7%9C%8B%3C/span%3E" STYLE="fork"/>
+      </node>
+      <node TEXT="车辆信息管理" ID="5vhivEZPKV" _mubu_text="%3Cspan%3E%E8%BD%A6%E8%BE%86%E4%BF%A1%E6%81%AF%E7%AE%A1%E7%90%86%3C/span%3E" STYLE="fork">
+        <node TEXT="新增车辆" ID="TC6gkUJ6Sp" _mubu_text="%3Cspan%3E%E6%96%B0%E5%A2%9E%E8%BD%A6%E8%BE%86%3C/span%3E" STYLE="fork"/>
+        <node TEXT="车辆信息修改" ID="0QOuBeAQTV" _mubu_text="%3Cspan%3E%E8%BD%A6%E8%BE%86%E4%BF%A1%E6%81%AF%E4%BF%AE%E6%94%B9%3C/span%3E" STYLE="fork"/>
+      </node>
+      <node TEXT="订单信息管理" ID="up7KxDART3" _mubu_text="%3Cspan%3E%E8%AE%A2%E5%8D%95%E4%BF%A1%E6%81%AF%E7%AE%A1%E7%90%86%3C/span%3E" STYLE="fork">
+        <node TEXT="订单查看" ID="lNQ0zCng3F" _mubu_text="%3Cspan%3E%E8%AE%A2%E5%8D%95%E6%9F%A5%E7%9C%8B%3C/span%3E" STYLE="fork"/>
+        <node TEXT="订单统计" ID="mNUNuLzqgv" _mubu_text="%3Cspan%3E%E8%AE%A2%E5%8D%95%E7%BB%9F%E8%AE%A1%3C/span%3E" STYLE="fork"/>
+      </node>
+      <node TEXT="后勤信息管理" ID="c1PKhwJp5i" _mubu_text="%3Cspan%3E%E5%90%8E%E5%8B%A4%E4%BF%A1%E6%81%AF%E7%AE%A1%E7%90%86%3C/span%3E" STYLE="fork">
+        <node TEXT="维修单查看" ID="qKqcMsZWkM" _mubu_text="%3Cspan%3E%E7%BB%B4%E4%BF%AE%E5%8D%95%E6%9F%A5%E7%9C%8B%3C/span%3E" STYLE="fork"/>
+        <node TEXT="提交维修单" ID="lASOmGYrv2" _mubu_text="%3Cspan%3E%E6%8F%90%E4%BA%A4%E7%BB%B4%E4%BF%AE%E5%8D%95%3C/span%3E" STYLE="fork"/>
+        <node TEXT="事故单查看" ID="8ZuXzDPHab" _mubu_text="%3Cspan%3E%E4%BA%8B%E6%95%85%E5%8D%95%E6%9F%A5%E7%9C%8B%3C/span%3E" STYLE="fork"/>
+        <node TEXT="提交事故单" ID="mP0tgPiHuC" _mubu_text="%3Cspan%3E%E6%8F%90%E4%BA%A4%E4%BA%8B%E6%95%85%E5%8D%95%3C/span%3E" STYLE="fork"/>
+      </node>
+    </node>
+    <node TEXT="后勤员" ID="5tvpJBcRAm" _mubu_text="%3Cspan%3E%E5%90%8E%E5%8B%A4%E5%91%98%3C/span%3E" STYLE="bubble" POSITION="left">
+      <node TEXT="车辆信息管理" ID="Oky2hPVQa0" _mubu_text="%3Cspan%3E%E8%BD%A6%E8%BE%86%E4%BF%A1%E6%81%AF%E7%AE%A1%E7%90%86%3C/span%3E" STYLE="fork">
+        <node TEXT="车辆信息修改" ID="XzBowz1aRM" _mubu_text="%3Cspan%3E%E8%BD%A6%E8%BE%86%E4%BF%A1%E6%81%AF%E4%BF%AE%E6%94%B9%3C/span%3E" STYLE="fork"/>
+      </node>
+      <node TEXT="后勤信息管理" ID="weSFhUOV0T" _mubu_text="%3Cspan%3E%E5%90%8E%E5%8B%A4%E4%BF%A1%E6%81%AF%E7%AE%A1%E7%90%86%3C/span%3E" STYLE="fork">
+        <node TEXT="维修单查看" ID="PCfIIdEe8l" _mubu_text="%3Cspan%3E%E7%BB%B4%E4%BF%AE%E5%8D%95%E6%9F%A5%E7%9C%8B%3C/span%3E" STYLE="fork"/>
+        <node TEXT="维修单统计" ID="J8OA4zWxdY" _mubu_text="%3Cspan%3E%E7%BB%B4%E4%BF%AE%E5%8D%95%E7%BB%9F%E8%AE%A1%3C/span%3E" STYLE="fork"/>
+        <node TEXT="维修单修改" ID="txtk8a0uwK" _mubu_text="%3Cspan%3E%E7%BB%B4%E4%BF%AE%E5%8D%95%E4%BF%AE%E6%94%B9%3C/span%3E" STYLE="fork"/>
+        <node TEXT="提交维修单" ID="bGnz2HsMmL" _mubu_text="%3Cspan%3E%E6%8F%90%E4%BA%A4%E7%BB%B4%E4%BF%AE%E5%8D%95%3C/span%3E" STYLE="fork"/>
+        <node TEXT="事故单查看" ID="CK2l0n1SPM" _mubu_text="%3Cspan%3E%E4%BA%8B%E6%95%85%E5%8D%95%E6%9F%A5%E7%9C%8B%3C/span%3E" STYLE="fork"/>
+        <node TEXT="事故单统计" ID="GawBhsqOW6" _mubu_text="%3Cspan%3E%E4%BA%8B%E6%95%85%E5%8D%95%E7%BB%9F%E8%AE%A1%3C/span%3E" STYLE="fork"/>
+        <node TEXT="事故单修改" ID="KHw7RE8cHB" _mubu_text="%3Cspan%3E%E4%BA%8B%E6%95%85%E5%8D%95%E4%BF%AE%E6%94%B9%3C/span%3E" STYLE="fork"/>
+        <node TEXT="提交事故单" ID="jSXAoUJVtr" _mubu_text="%3Cspan%3E%E6%8F%90%E4%BA%A4%E4%BA%8B%E6%95%85%E5%8D%95%3C/span%3E" STYLE="fork"/>
+      </node>
+    </node>
+    <node TEXT="客户" ID="aU5dIkIdPw" _mubu_text="%3Cspan%3E%E5%AE%A2%E6%88%B7%3C/span%3E" STYLE="bubble" POSITION="left">
+      <node TEXT="注册与登录" ID="Scx3vn2oir" _mubu_text="%3Cspan%3E%E6%B3%A8%E5%86%8C%E4%B8%8E%E7%99%BB%E5%BD%95%3C/span%3E" STYLE="fork">
+        <node TEXT="新用户注册" ID="vH4GGikHXn" _mubu_text="%3Cspan%3E%E6%96%B0%E7%94%A8%E6%88%B7%E6%B3%A8%E5%86%8C%3C/span%3E" STYLE="fork"/>
+        <node TEXT="用户登录" ID="dz6LOsTKyL" _mubu_text="%3Cspan%3E%E7%94%A8%E6%88%B7%E7%99%BB%E5%BD%95%3C/span%3E" STYLE="fork"/>
+      </node>
+      <node TEXT="选择车辆" ID="2GQTe2ZDnt" _mubu_text="%3Cspan%3E%E9%80%89%E6%8B%A9%E8%BD%A6%E8%BE%86%3C/span%3E" STYLE="fork">
+        <node TEXT="车辆信息查看" ID="ACJ1D30aOA" _mubu_text="%3Cspan%3E%E8%BD%A6%E8%BE%86%E4%BF%A1%E6%81%AF%E6%9F%A5%E7%9C%8B%3C/span%3E" STYLE="fork"/>
+      </node>
+      <node TEXT="选择门店" ID="RcTi5aPYVr" _mubu_text="%3Cspan%3E%E9%80%89%E6%8B%A9%E9%97%A8%E5%BA%97%3C/span%3E" STYLE="fork">
+        <node TEXT="门店信息查看" ID="sDLfGakMq5" _mubu_text="%3Cspan%3E%E9%97%A8%E5%BA%97%E4%BF%A1%E6%81%AF%E6%9F%A5%E7%9C%8B%3C/span%3E" STYLE="fork"/>
+      </node>
+      <node TEXT="我的订单" ID="6jLbkBA023" _mubu_text="%3Cspan%3E%E6%88%91%E7%9A%84%E8%AE%A2%E5%8D%95%3C/span%3E" STYLE="fork">
+        <node TEXT="订单提交" ID="WsTqZvGUu6" _mubu_text="%3Cspan%3E%E8%AE%A2%E5%8D%95%E6%8F%90%E4%BA%A4%3C/span%3E" STYLE="fork"/>
+        <node TEXT="订单查看" ID="XDvsudfNTU" _mubu_text="%3Cspan%3E%E8%AE%A2%E5%8D%95%E6%9F%A5%E7%9C%8B%3C/span%3E" STYLE="fork"/>
+        <node TEXT="订单统计" ID="FFXxX87wjp" _mubu_text="%3Cspan%3E%E8%AE%A2%E5%8D%95%E7%BB%9F%E8%AE%A1%3C/span%3E" STYLE="fork"/>
+      </node>
+      <node TEXT="故障保修" ID="L2FkChpsZL" _mubu_text="%3Cspan%3E%E6%95%85%E9%9A%9C%E4%BF%9D%E4%BF%AE%3C/span%3E" STYLE="fork">
+        <node TEXT="事故单提交" ID="XQnZXNFnYA" _mubu_text="%3Cspan%3E%E4%BA%8B%E6%95%85%E5%8D%95%E6%8F%90%E4%BA%A4%3C/span%3E" STYLE="fork"/>
+        <node TEXT="故障单提交" ID="tN1JDcxHap" _mubu_text="%3Cspan%3E%E6%95%85%E9%9A%9C%E5%8D%95%E6%8F%90%E4%BA%A4%3C/span%3E" STYLE="fork"/>
+        <node TEXT="处理结果查看" ID="EA83hi8xlD" _mubu_text="%3Cspan%3E%E5%A4%84%E7%90%86%E7%BB%93%E6%9E%9C%E6%9F%A5%E7%9C%8B%3C/span%3E" STYLE="fork"/>
+      </node>
+      <node TEXT="我的信息" ID="e3pK6YS4Ly" _mubu_text="%3Cspan%3E%E6%88%91%E7%9A%84%E4%BF%A1%E6%81%AF%3C/span%3E" STYLE="fork">
+        <node TEXT="用户信息管理" ID="WR8n7fewjz" _mubu_text="%3Cspan%3E%E7%94%A8%E6%88%B7%E4%BF%A1%E6%81%AF%E7%AE%A1%E7%90%86%3C/span%3E" STYLE="fork"/>
+      </node>
+    </node>
+  </node>
+</map>
